d2262c57a315b7a0d9ce1a57d3600ee31f297e69
Tibo <thibaut.coquelet@gmail.com>
2021-11-09 02:37:22 +0100

    Improve landing page (design + api)

--- src/Components/Card/FirstCardLandingPage.jsx
+++ src/Components/Card/FirstCardLandingPage.jsx
@@ -1,14 +1,72 @@
+import { motion } from "framer-motion";
+
 const FirstCardLandingPage = () => {
     return (
-        <div class="w-screen bg-white rounded-lg shadow-lg ">
+        <div class="w-screen rounded-lg shadow-lg bg-gradient-to-r from-yellow-500 to-red-500">
             <div class="grid place-items-center">
-                <img src="https://cdn.pixabay.com/photo/2014/07/11/20/14/book-390277_1280.jpg" alt="" class="rounded-t-lg" />
+                <img src="https://cdn.pixabay.com/photo/2014/07/11/20/14/book-390277_1280.jpg" width="55%" alt="" class="rounded-t-lg" />
                 <div class="p-6">
-                <div class="grid place-items-center">
-                    <h2 class="font-bold mb-2 text-2xl text-purple-800">Envie d'évasion ?
-                    </h2>
-                </div>
-                    <p class="text-purple-700 mb-2">Utilisez notre moteur de recherche pour trouver le livre qui vous convient !</p>
+                    <div class="grid place-items-center">
+                        <motion.div initial="hidden" animate="visible" variants={{
+                            hidden: {
+                                scale: .8,
+                                opacity: 0
+                            },
+                            visible: {
+                                scale: 1,
+                                opacity: 1,
+                                transition: {
+                                    delay: .4
+                                }
+                            }
+                        }}>
+                            <h2 class="font-bold mb-2 text-4xl text-pink-900 mb-2">Envie d'évasion ?
+                            </h2>
+                        </motion.div>
+                    </div>
+                    <motion.div initial="hidden" animate="visible" variants={{
+                        hidden: {
+                            scale: .8,
+                            opacity: 0
+                        },
+                        visible: {
+                            scale: 1,
+                            opacity: 1,
+                            transition: {
+                                delay: .6
+                            }
+                        }
+
+                    }}>
+                        <p class="text-2xl text-pink-900 mb-2">Utilisez notre moteur de recherche pour trouver le livre qui vous convient !</p>
+                    </motion.div>
+                    <div class="grid place-items-center">
+                        <motion.div initial="hidden" animate="visible" variants={{
+                            hidden: {
+                                scale: .8,
+                                opacity: 0
+                            },
+                            visible: {
+                                scale: 1,
+                                opacity: 1,
+                                transition: {
+                                    delay: .8
+                                }
+                            }
+
+                        }}>
+                            <motion.a initial="beforeHover" whileHover="onHover" variants={{
+                                onHover: {
+                                    height: 300,
+                                transition: {
+                                    type: "tween"
+                                }
+                            },
+                            }}>
+                            <a href="#" class="text-pink-800 hover:text-purple-900 underline text-1xl">Chercher votre nouveau livre</a>
+                            </motion.a>
+                        </motion.div>
+                    </div>
                 </div>
             </div>
 
--- src/Components/pages/Home/Home.jsx
+++ src/Components/pages/Home/Home.jsx
@@ -1,12 +1,63 @@
 import { CarouselCardData } from "../../Carousel/CarouselCardDate";
 import CarouselLandingPage from "../../Carousel/CarouselLandingPage";
+import { FaArrowCircleDown } from "react-icons/fa";
+import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
+import CardBooksLandingPage from "../../Card/CardBooksLandingPage";
 
 const Home = () => {
+    const [books, setBooks] = useState(null);
+    const api_key=process.env.REACT_APP_CLE_API_NYTIMES;
+    const showBestSellers = async () => {
+        const url = "https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json?api-key="+api_key;
+        const response = await fetch(url);
+        const data = await response.json();
+        const results = data.results;
+        console.log(results);
+        setBooks(results);
+        console.log(books)
+    }
+
+    useEffect(async () => {
+        showBestSellers();
+    }, []);
+
     return (
-        <div className="pt-20">
+        <div className="pt-19">
             <div class="grid place-items-center">
                 <CarouselLandingPage cards={CarouselCardData} />
             </div>
+            <div class="grid place-items-center">
+                <button onClick={() => { alert('clicked') }}> <FaArrowCircleDown size={30} style={{ fill: 'red' }} /></button>
+            </div>
+            <div class="grid place-items-center pt-10">
+                <motion.div
+                    initial={{ opacity: 0, y: '-100px' }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{
+                        type: 'spring',
+                        repeat: Infinity,
+                        duration: 2,
+                        bounce: 0.7,
+                        repeatType: 'reverse'
+                    }}
+                >
+                    <p class="text-7xl text-yellow-700">Découvrez les best sellers de cette année</p>
+                </motion.div>
+            </div>
+            {<div class="container my-12 mx-auto pt-10 md:px-12">
+                {books && (
+                    <div class="flex flex-wrap -mx-1 lg:-mx-4">
+                        {books.map((book, index) => (
+                            <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3" key={index}>
+                                <CardBooksLandingPage title={book.title} author={book.author} description={book.description} />
+                            </div>
+                        ))}
+                    </div>
+                )}
+            </div>}
+
+
         </div>
     )
 }
